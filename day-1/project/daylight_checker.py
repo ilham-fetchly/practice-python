66629ff96568fb2ff9ced702915949d1fd426d42
Ilham Fetchly <ilham@fetch.ly>
2023-02-25 09:59:56 +0000

    Fix lint format

--- day-1/project/daylight_checker.py
+++ day-1/project/daylight_checker.py
@@ -1,29 +1,57 @@
 ## Personal practice project ##
 ## Program to compare the hour input with current hour time ##
+from datetime import datetime
 print("Welcome to the Daylight Checker.")
 
-from datetime import datetime
 name = input('What is your name? ')
+print('\nHi ' + name + ', input a number 1 to 24 according to your local time')
+
+# Get the current time
+now = datetime.now()
+time_hour = now.hour
+
+# Format reference at https://www.geeksforgeeks.org/python-strftime-function/
+current_time = now.strftime("%a %b %d,%Y %I:%M %p")
+
+if time_hour == 0:
+    time_hour = 24
+
+
+# Function to convert the string input to the integer value
+def input_number():
+    input_ = input('What time is it now, ' + name + '? ')
+    input_x = float(input_)
+    input_time = int(input_x)
+    return input_time
+
+
+# Function to determine the nearby timetable
+def nearby_time():
+    if time_hour >= 4 and time_hour < 5:
+        print("But it's almost morning now!")
+    elif time_hour >= 11 and time_hour < 12:
+        print("But it's almost afternoon now!")
+    elif time_hour >= 16 and time_hour < 17:
+        print("But it's almost evening now!")
+    elif time_hour >= 21 and time_hour < 22:
+        print("But it's almost night now!")
+    else:
+        None
+    print("Current time:", current_time, end="\n")
+
 
 def start():
-    # Get the current time
-    now = datetime.now()
-    time_hour = now.hour
-    # Format the date and time using strftime function
-    # Format reference at https://www.geeksforgeeks.org/python-strftime-function/
-    current_time = now.strftime("%a %b %d,%Y %I:%M %p")
-    print('\nHi ' + name + ', input numbers 1 to 24 according to your local time')
-    input_ = input('What time is it now, ' + name + '? ')
-    # Validation input number
-    while not input_.isnumeric():
-        input_ = input("Please input a number: ")
-    
-    # Convert the string number input to the integer value
-    input_time = int(input_)
+    # Input validation
+    try:
+        input_time = input_number()
+    except ValueError:
+        print("Wrong input! Please input a number 1 to 24.")
+        start()
 
     # The else-if operator determines the timetable.
     if input_time > 24:
-        print("Wrong input! Please input numbers 1 to 24")
+        print("Wrong input! Please input a number 1 to 24")
+        start()
     else:
         if time_hour >= 5 and time_hour < 12:
             day = 'morning'
@@ -31,24 +59,8 @@ def start():
             day = 'afternoon'
         elif time_hour >= 17 and time_hour < 22:
             day = 'evening'
-        else: day = 'night'
-
-        # Function to determine the nearby timetable
-        def nearby_time():
-            print('Good ' + day + ', ' + name + '!')
-            if time_hour >= 4 and time_hour < 5:
-                print("But it's almost morning now!")
-            elif time_hour >= 11 and time_hour < 12:
-                print("But it's almost afternoon now!")
-            elif time_hour >= 16 and time_hour < 17:
-                print("But it's almost evening now!")
-            elif time_hour >= 21 and time_hour < 22:
-                print("But it's almost night now!")
-            else: None
-            print("Current time:", current_time, end="\n")
-
-        if time_hour == 0:
-            time_hour = 24
+        else:
+            day = 'night'
 
         if input_time == 0:
             input_time = 24
@@ -56,16 +68,21 @@ def start():
         if input_time == time_hour:
             print("Right, it's " + str(input_time) + " at " + day + ' now.')
         else:
-            print("No, Now it's", time_hour, 'at', day + '!', end=' ')
+            print("No, Now it's", time_hour, 'at', day + '!')
 
+        print('Good ' + day + ', ' + name + '!')
         nearby_time()
-    restart_app()
+    restart()
 
-def restart_app():
+
+def restart():
     restart = input("\nDo you want to restart the program?(y/n) ")
     if restart == "y" or restart == "Y":
         start()
-    else: print("See you")
+    else:
+        print("Bye,", name + "!")
+        exit()
+
 
 # Starting the program
 start()
