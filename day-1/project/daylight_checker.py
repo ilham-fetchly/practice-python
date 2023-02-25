f9d94e99febe2d19669ff97579a82abed118a451
Ilham Fetchly <ilham@fetch.ly>
2023-02-25 09:37:17 +0800

    Add input validation

--- day-1/project/daylight_checker.py
+++ day-1/project/daylight_checker.py
@@ -13,44 +13,52 @@ def start():
     # Format reference at https://www.geeksforgeeks.org/python-strftime-function/
     current_time = now.strftime("%a %b %d,%Y %I:%M %p")
     print('\nHi ' + name + ', input numbers 1 to 24 according to your local time')
-    # Convert the string input to the integer value using the 'int' method
-    input_time = int(input('What time is it now, ' + name + '? '))
+    input_ = input('What time is it now, ' + name + '? ')
+    # Validation input number
+    while not input_.isnumeric():
+        input_ = input("Please input a number: ")
+    
+    # Convert the string number input to the integer value
+    input_time = int(input_)
 
     # The else-if operator determines the timetable.
-    if time_hour >= 5 and time_hour < 12:
-        day = 'morning'
-    elif time_hour >= 12 and time_hour < 17:
-        day = 'afternoon'
-    elif time_hour >= 17 and time_hour < 22:
-        day = 'evening'
-    else: day = 'night'
-
-    # Function to determine the nearby timetable
-    def nearby_time():
-        print('Good ' + day + ', ' + name + '!')
-        if time_hour >= 4 and time_hour < 5:
-            print("But it's almost morning now!")
-        elif time_hour >= 11 and time_hour < 12:
-            print("But it's almost afternoon now!")
-        elif time_hour >= 16 and time_hour < 17:
-            print("But it's almost evening now!")
-        elif time_hour >= 21 and time_hour < 22:
-            print("But it's almost night now!")
-        else: None
-        print("Current time:", current_time, end="\n")
-
-    if time_hour == 0:
-        time_hour = 24
-
-    if input_time == 0:
-        input_time = 24
-
-    if input_time == time_hour:
-        print("Right, it's " + str(input_time) + " at " + day + ' now.')
+    if input_time > 24:
+        print("Wrong input! Please input numbers 1 to 24")
     else:
-        print("No, Now it's", time_hour, 'at', day + '!', end=' ')
+        if time_hour >= 5 and time_hour < 12:
+            day = 'morning'
+        elif time_hour >= 12 and time_hour < 17:
+            day = 'afternoon'
+        elif time_hour >= 17 and time_hour < 22:
+            day = 'evening'
+        else: day = 'night'
 
-    nearby_time()
+        # Function to determine the nearby timetable
+        def nearby_time():
+            print('Good ' + day + ', ' + name + '!')
+            if time_hour >= 4 and time_hour < 5:
+                print("But it's almost morning now!")
+            elif time_hour >= 11 and time_hour < 12:
+                print("But it's almost afternoon now!")
+            elif time_hour >= 16 and time_hour < 17:
+                print("But it's almost evening now!")
+            elif time_hour >= 21 and time_hour < 22:
+                print("But it's almost night now!")
+            else: None
+            print("Current time:", current_time, end="\n")
+
+        if time_hour == 0:
+            time_hour = 24
+
+        if input_time == 0:
+            input_time = 24
+
+        if input_time == time_hour:
+            print("Right, it's " + str(input_time) + " at " + day + ' now.')
+        else:
+            print("No, Now it's", time_hour, 'at', day + '!', end=' ')
+
+        nearby_time()
     restart_app()
 
 def restart_app():
