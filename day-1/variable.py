7d1542a1930a013f4ee842f9eafe871f2ac26c9e
ilham <ilham@fetch.ly>
2023-02-26 02:39:00 +0000

    Add comments

--- day-1/variable.py
+++ day-1/variable.py
@@ -1,9 +1,9 @@
-a = input("a: ")
-b = input("b: ")
+a = input("a: ")  # input 3
+b = input("b: ")  # input 5
 
 c = a
 a = b
 b = c
 
-print("a: " + a)
-print("b: " + b)
+print("a: " + a)  # expected output 5
+print("b: " + b)  # expected output 3
